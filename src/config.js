cb8fae6e34c7410c26b2b6276af151a8fceee7f1
jhollcr <jake.hollcroft@williams.com>
2026-05-27 08:11:49 -0500

    Trigger rebuild for env vars

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  GITHUB_TOKEN: process.env.REACT_APP_GITHUB_TOKEN || "",
-  GIST_ID: process.env.REACT_APP_GIST_ID || "",
+  GITHUB_TOKEN: process.env.REACT_APP_GITHUB_TOKEN,
+  GIST_ID: process.env.REACT_APP_GIST_ID,
   GIST_FILENAME: "budget-data.json",
 };
 
